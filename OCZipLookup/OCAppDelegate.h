e55f2722cbd1c6c0f7f082061ccea61666ed0485
Oliver <ocrickard@gmail.com>
2012-04-18 16:48:57 -0700

    Initial Commit

---
+++ OCZipLookup/OCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OCAppDelegate.h
+//  OCZipLookup
+//
+//  Created by Oliver Rickard on 4/18/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OCViewController;
+
+@interface OCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) OCViewController *viewController;
+
+@end
---
+++ OCZipLookup/OCViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCViewController.h
+//  OCZipLookup
+//
+//  Created by Oliver Rickard on 4/18/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCViewController : UIViewController
+
+@end
